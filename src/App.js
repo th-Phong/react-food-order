f012090044c4c6863dafba7ba03230fd6a98c46a
thPhong123 <hongphong120300@gmail.com>
2021-09-30 15:02:01 +0700

    final v1

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Meals from "./components/Meals/Meals";
+
+import Header from "./components/Layout/Header";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+  const [cartIsShow, setCartIsShow] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShow(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShow(false);
+  };
+  return (
+    <CartProvider>
+      {cartIsShow && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
